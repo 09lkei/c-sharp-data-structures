9dbc6cc34978d9ad24c2d198bdef1c84a1184188
09lkei <09lkei@brightoncollege.net>
2023-03-20 20:20:27 +0000

    fix

---
+++ Csharp circular queues/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+    queue<string> myQueue = new queue<string>(5);
+    Console.WriteLine(myQueue.dequeue());
+    myQueue.enqueue("hello");
+    myQueue.enqueue("hello2");
+    myQueue.enqueue("hello3");
+    myQueue.enqueue("hello4");
+    myQueue.enqueue("hello5");
+    myQueue.enqueue("hello6");
+    myQueue.dequeue();
+    myQueue.returnQueue();
+  }
+}
+
+class queue<T> {
+  private int maxSize = -1;
+  private T[] data;
+  private int back = 0;
+  private int front = 0;
+  public queue(int maxSize) {
+    this.maxSize = maxSize;
+    data = new T[maxSize];
+  }
+
+  public void enqueue(T newItem) {
+    if ((back-front)<maxSize) {
+      data[back] = newItem;
+      if (back<maxSize*2) {
+        back++;
+      }
+      else
+      {
+        back = 0;
+        front -= maxSize;
+      }
+    } else {
+      Console.WriteLine("Full queue!");
+    }
+  }
+
+  public T dequeue() {
+    if (back-front>0)
+    {
+      T removed = data[front];
+      if (front < maxSize)
+      {
+        front++;
+      }
+      else
+      {
+        front = 0;
+        back -= maxSize;
+      }
+      return removed;
+    } else {
+      return default(T);
+    }
+  }
+
+  public void returnQueue() {
+    foreach (T s in data) {
+      Console.WriteLine(s);
+    }
+  }
+}
